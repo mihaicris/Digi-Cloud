0b6e9c5270a357a4df77678ce3f50c3494a6abe1
Mihai Cristescu <mihai.cristescu@gmail.com>
2016-09-15 21:20:31 +0300

    Initial Commit

---
+++ Digi Cloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Digi Cloud
+//
+//  Created by Mihai Cristescu on 15/09/16.
+//  Copyright © 2016 Mihai Cristescu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
