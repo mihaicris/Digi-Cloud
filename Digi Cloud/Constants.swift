2da13f0c2fa1c47a0c965f835b8542b8f8c7acdb
Mihai Cristescu <mihai.cristescu@gmail.com>
2016-09-15 21:37:50 +0300

    Define api main url

---
+++ Digi Cloud/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  test
+//
+//  Created by Mihai Cristescu on 15/09/16.
+//  Copyright © 2016 Mihai Cristescu. All rights reserved.
+//
+
+import Foundation
+
+let api_base = "https://storage.rcs-rds.ro"
