c52b1cc6a35d8f882601ef99a87fb1c0269590ae
Mihai Cristescu <mihai.cristescu@gmail.com>
2016-09-19 11:44:03 +0300

    Prepare for navigation in the mount structure

---
+++ Digi Cloud/FilesTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FilesTableViewController.swift
+//  Digi Cloud
+//
+//  Created by Mihai Cristescu on 19/09/16.
+//  Copyright © 2016 Mihai Cristescu. All rights reserved.
+//
+
+import UIKit
+
+class FilesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FileCell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+
+}
