00b1efdd71d9ded96c3c76872cd3f6e44ea40136
Mihai Cristescu <mihai.cristescu@gmail.com>
2016-10-18 22:56:36 +0300

    Remove main storyboard

--- Digi Cloud/AppDelegate.swift
+++ Digi Cloud/AppDelegate.swift
@@ -10,9 +10,9 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -24,4 +24,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 }
-
