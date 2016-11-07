f2b9d2d696d8053efa48141317467968db13acbb
Mihai Cristescu <mihai.cristescu@gmail.com>
2016-11-07 17:06:57 +0200

    Configure FolderInfoViewController

--- Digi Cloud/FolderInfoViewController.swift
+++ Digi Cloud/FolderInfoViewController.swift
@@ -14,6 +14,7 @@ class FolderInfoViewController: UITableViewController {
     fileprivate var element: File
     fileprivate var rightBarButton: UIBarButtonItem!
     fileprivate var messageLabel: UILabel!
+    fileprivate var deleteButton: UIButton!
 
     init(element: File) {
         self.element = element
@@ -40,36 +41,85 @@ class FolderInfoViewController: UITableViewController {
 
     }
 
-
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return indexPath.section == 0 ? 200 : 80
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         cell.selectionStyle = .none
 
+        if indexPath.section == 0 {
+            let folderIcon: UIImageView = {
+                let imageName = element.type == "dir" ? "FolderIcon" : "FileIcon"
+                let imageView = UIImageView(image: UIImage(named: imageName))
+                imageView.contentMode = .scaleAspectFit
+                return imageView
+            }()
+
+            let folderName: UILabel = {
+                let label = UILabel()
+                label.text = element.name
+                return label
+            }()
+
+            cell.contentView.addSubview(folderIcon)
+            cell.contentView.addSubview(folderName)
+            cell.contentView.addConstraints(with: "H:|-20-[v0(26)]-12-[v1]-12-|", views: folderIcon, folderName)
+            cell.contentView.addConstraints(with: "V:|-20-[v0(26)]", views: folderIcon)
+            cell.contentView.addConstraints(with: "V:|-20-[v0]", views: folderName)
+
+        } else {
+            deleteButton = UIButton(type: UIButtonType.system)
+            deleteButton.layer.borderColor = UIColor.red.cgColor
+            deleteButton.layer.cornerRadius = 5
+            deleteButton.layer.borderWidth = 1/UIScreen.main.scale
+            deleteButton.setTitle(NSLocalizedString("  Delete Folder  ", comment: "Title for Button"), for: .normal)
+            deleteButton.setTitleColor(.red, for: .normal)
+            deleteButton.setTitleColor(.gray, for: .disabled)
+            deleteButton.addTarget(self, action: #selector(handleDelete), for: .touchUpInside)
+
+            //constraints
+            deleteButton.translatesAutoresizingMaskIntoConstraints = false
+            cell.contentView.addSubview(deleteButton)
+            deleteButton.centerXAnchor.constraint(equalTo: cell.contentView.centerXAnchor).isActive = true
+            deleteButton.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor).isActive = true
+        }
         return cell
     }
+
+//    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+//        if section == 1 {
+//            let footerView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+//            footerView.backgroundColor = .magenta
+//            return footerView
+//        } else {
+//            return nil
+//        }
+//    }
 
     fileprivate func setupViews() {
 
         messageLabel = {
             let label = UILabel()
             label.textAlignment = .center
+            label.text = "Test"
             label.font = UIFont.systemFont(ofSize: 14)
             label.textColor = .darkGray
-            label.alpha = 0.0
+            label.alpha = 1
             return label
         }()
 
-        tableView.addSubview(messageLabel)
-        tableView.addConstraints(with: "V:|-100-[v0]|", views: messageLabel)
-        tableView.centerXAnchor.constraint(equalTo: messageLabel.centerXAnchor).isActive = true
         tableView.isScrollEnabled = false
 
         rightBarButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(handleDone))
-
         self.navigationItem.setRightBarButton(rightBarButton, animated: false)
 
         self.title = NSLocalizedString("Folder information", comment: "Title")
@@ -80,7 +130,7 @@ class FolderInfoViewController: UITableViewController {
     }
 
     @objc fileprivate func handleDelete() {
-
+        deleteButton.isEnabled = false
     }
 
     #if DEBUG
