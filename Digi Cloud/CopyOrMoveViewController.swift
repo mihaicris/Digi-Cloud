8650b300142fd2a6b1a1b5cac45457f5002fc7b6
Mihai Cristescu <mihai.cristescu@gmail.com>
2016-11-16 16:25:07 +0200

    Work on copy/move view controller

--- Digi Cloud/CopyOrMoveViewController.swift
+++ Digi Cloud/CopyOrMoveViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CopyOrMoveViewController: UIViewController {
+class CopyOrMoveViewController: UITableViewController {
 
     var onFinish: ((Void) -> Void)?
 
@@ -33,18 +33,29 @@ class CopyOrMoveViewController: UIViewController {
     private func setupViews() {
         view.backgroundColor = UIColor.white
 
+        var buttonTitle: String
+
         switch action {
         case .copy:
             self.title = element.type == "file" ? NSLocalizedString("Copy File", comment: "Window title") : NSLocalizedString("Copy Folder", comment: "Window title")
+            buttonTitle = NSLocalizedString("Copy here", comment: "Button Title")
         case .move:
             self.title = element.type == "file" ? NSLocalizedString("Move File", comment: "Window title") : NSLocalizedString("Move Folder", comment: "Window title")
+            buttonTitle = NSLocalizedString("Move here", comment: "Button Title")
         default:
             return
         }
 
         let rightButton = UIBarButtonItem(title: NSLocalizedString("Cancel", comment: "Button Title"), style: UIBarButtonItemStyle.done, target: self, action: #selector(handleDone))
-
         navigationItem.setRightBarButton(rightButton, animated: false)
+
+        navigationController?.isToolbarHidden = false
+
+        let toolBarItems = [UIBarButtonItem(title: NSLocalizedString("New Folder", comment: "Button Title"), style: .plain, target: self, action: #selector(handleNewFolder)),
+                            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+                            UIBarButtonItem(title: buttonTitle, style: .plain, target: self, action: #selector(handleCopyOrMove))]
+
+        self.setToolbarItems(toolBarItems, animated: true)
 
     }
 
@@ -52,17 +63,24 @@ class CopyOrMoveViewController: UIViewController {
         self.onFinish?()
     }
 
+    @objc private func handleNewFolder() {
+        self.onFinish?()
+    }
+
     @objc private func handleCopyOrMove() {
-        guard let mountId = DigiClient.shared.currentMount else { return }
+
+        guard let currentMount = DigiClient.shared.currentMount else { return }
         guard let currentPath = DigiClient.shared.currentPath.last else { return }
 
-        let sourcePath = currentPath + element.name
-        let destinationPath = currentPath + element.name  // TODO: Update with selected destination path (without element name inside)
+        let elementSourcePath = currentPath + element.name
+        let destinationMount = currentMount  // TODO: Update with destination mount
+        let elementDestinationPath = currentPath + element.name  // TODO: Update with selected destination path (without element name inside)
 
+        return
         DigiClient.shared.copyOrMoveElement(action:             action,
-                                            path:               sourcePath,
-                                            toMountId:          mountId,
-                                            toPath:             destinationPath,
+                                            path:               elementSourcePath,
+                                            toMountId:          destinationMount,
+                                            toPath:             elementDestinationPath,
                                             completionHandler:  {(statusCode, error) in return })
     }
 }
