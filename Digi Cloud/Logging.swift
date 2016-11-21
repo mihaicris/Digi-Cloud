0d72e37403d4671df086eb7b9a85bf8b3a0eade0
Mihai Cristescu <mihai.cristescu@gmail.com>
2016-11-21 20:29:55 +0200

    Update log function

--- Digi Cloud/Logging.swift
+++ Digi Cloud/Logging.swift
@@ -8,14 +8,15 @@
 
 import Foundation
 
-public func DLog<T>( object: @autoclosure () -> T, _ file: String = #file, _ function: String = #function, _ line: Int = #line) {
+public func DLog<T>( name: String, object: @autoclosure () -> T, _ file: String = #file, _ function: String = #function, _ line: Int = #line) {
     #if DEBUG
         let queue = Thread.isMainThread ? "Main (UI)" : "Background"
         print("===================================================")
-        print("File:     \(file.components(separatedBy: "/").last!)")
-        print("Function: \(function)")
-        print("Line:     \(line)")
-        print("Thread:   \(queue)")
-        print("Object:   \(object())")
+        print("File:        \(file.components(separatedBy: "/").last!)")
+        print("Function:    \(function)")
+        print("Line:        \(line)")
+        print("Thread:      \(queue)")
+        print("Object:      \(name)")
+        print("\(object())")
     #endif
 }
