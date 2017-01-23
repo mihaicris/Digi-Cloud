fe50d6d6f6f89b291c01e9a421d8293b0e00ad6d
Mihai Cristescu <mihai.cristescu@gmail.com>
2017-01-23 22:46:44 +0200

    Add tests target

---
+++ Digi Cloud Tests/DigiCloudTests.swift
@@ -0,0 +1,25 @@
+//
+//  Digi_Cloud_Tests.swift
+//  Digi Cloud Tests
+//
+//  Created by Mihai Cristescu on 23/01/17.
+//  Copyright © 2017 Mihai Cristescu. All rights reserved.
+//
+
+import XCTest
+
+class DigiCloudTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+        XCTAssert(true)
+    }
+
+}
