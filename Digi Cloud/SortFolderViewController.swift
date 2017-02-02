79a09865d5b562cd7403147cfccf12b3c2929230
Mihai Cristescu <mihai.cristescu@gmail.com>
2017-02-02 14:09:14 +0200

    Change title

--- Digi Cloud/SortFolderViewController.swift
+++ Digi Cloud/SortFolderViewController.swift
@@ -95,7 +95,7 @@ class SortFolderViewController: UITableViewController {
 
     fileprivate func setInitialActionNames() {
         contextMenuSortActions = [
-            NSLocalizedString("Folders first", comment: "Switch Title"),
+            NSLocalizedString("Directories first", comment: "Switch Title"),
             NSLocalizedString("Sort by Name", comment: "Selection Title"),
             NSLocalizedString("Sort by Date", comment: "Selection Title"),
             NSLocalizedString("Sort by Size", comment: "Selection Title"),
