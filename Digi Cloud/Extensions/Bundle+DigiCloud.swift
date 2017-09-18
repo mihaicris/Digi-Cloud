0ff81c8285eeca87489cdd084cf05eb5ce5676f1
Mihai Cristescu <mihai.cristescu@gmail.com>
2017-09-18 11:45:23 +0300

    Rename file

--- Digi Cloud/Extensions/Bundle+DigiCloud.swift
+++ Digi Cloud/Extensions/Bundle+DigiCloud.swift
@@ -1,5 +1,5 @@
 //
-//  UIApplication+DigiCloud.swift
+//  Bundle+DigiCloud.swift
 //  Digi Cloud
 //
 //  Created by Mihai Cristescu on 17/01/17.
@@ -9,20 +9,20 @@
 import Foundation
 
 extension Bundle {
-    
+
     var versionNumber: String? {
         return infoDictionary?["CFBundleShortVersionString"] as? String
     }
-    
+
     var buildNumber: String? {
         return infoDictionary?["CFBundleVersion"] as? String
     }
-    
+
     var prettyVersionString: String {
         let ver = versionNumber ?? NSLocalizedString("Unknown", comment: "")
         let build = buildNumber ?? "0"
         let format = NSLocalizedString("Digi Cloud\nVersion %@ (%@)", comment: "")
         return  String.localizedStringWithFormat(format, ver, build)
     }
-    
+
 }
