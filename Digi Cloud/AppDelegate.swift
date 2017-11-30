c1d1b18a519ee82d12e894405cd7e0fc2f8f759e
Mihai Cristescu <mihai.cristescu@gmail.com>
2017-11-30 18:19:17 +0200

    Add Crashlytics repports

--- Digi Cloud/AppDelegate.swift
+++ Digi Cloud/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
+        // Start Crashlytics
+        Fabric.with([Crashlytics.self])
+        
         // Get a window
         window = UIWindow(frame: UIScreen.main.bounds)
 
